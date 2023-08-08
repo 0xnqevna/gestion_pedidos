9761d29c906c717ed07e2df93151c845776caa34
0xnqevna <nqevna@gmail.com>
2023-08-08 11:31:50 +0200

    inicio proyecto Spring

---
+++ src/main/java/com/ajgestion/gestionpedidos/GestionDePedidosApplication.java
@@ -0,0 +1,13 @@
+package com.ajgestion.gestionpedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDePedidosApplication.class, args);
+	}
+
+}
